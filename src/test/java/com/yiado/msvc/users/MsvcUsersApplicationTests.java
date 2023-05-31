0ced0701352291f56f9b6be9be4c203b66cc15d4
YmendezM <ysmendezm@gmail.com>
2023-05-31 10:19:18 -0400

    Create micro service users

---
+++ src/test/java/com/yiado/msvc/users/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiado.msvc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
