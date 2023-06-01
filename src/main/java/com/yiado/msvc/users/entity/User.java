b9eb6aaaa1ecf617be88b4091744b3da85f53203
YmendezM <ysmendezm@gmail.com>
2023-05-31 23:44:38 -0400

    Create entity user

---
+++ src/main/java/com/yiado/msvc/users/entity/User.java
@@ -0,0 +1,41 @@
+package com.yiado.msvc.users.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String Email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
