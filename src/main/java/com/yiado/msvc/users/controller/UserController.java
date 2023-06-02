a437fcdeb629407323d6d3c345863b1198079c93
YmendezM <ysmendezm@gmail.com>
2023-06-02 09:19:11 -0400

    Validate request users

--- src/main/java/com/yiado/msvc/users/controller/UserController.java
+++ src/main/java/com/yiado/msvc/users/controller/UserController.java
@@ -5,10 +5,15 @@ import com.yiado.msvc.users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -33,13 +38,18 @@ public class UserController {
 
     @PostMapping
     //@ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<?>  create(@RequestBody User user){
+    public ResponseEntity<?>  create(@Valid @RequestBody User user, BindingResult result){
+        ResponseEntity<Map<String, String>> errors = getMapResponseEntity(result);
+        if (errors != null) return errors;
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
     @PutMapping("/{id}")
     //@ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<?>  update(@RequestBody User user, @PathVariable Long id){
+    public ResponseEntity<?>  update(@Valid @RequestBody User user, BindingResult result, @PathVariable Long id){
+        ResponseEntity<Map<String, String>> errors = getMapResponseEntity(result);
+        if (errors != null) return errors;
+
         Optional<User> userOptional =  userService.findById(id);
         if(userOptional.isPresent()){
            User userCurrent =  userOptional.get();
@@ -60,6 +70,18 @@ public class UserController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    private static ResponseEntity<Map<String, String>> getMapResponseEntity(BindingResult result) {
+        if(result.hasErrors()){
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                errors.put(err.getField(), " " + err.getField() + " " + err.getDefaultMessage());
+            });
+            return ResponseEntity.badRequest().body(errors);
+        }
+        return null;
+    }
+
 
 
 }
