57c4bfcc4c1e720b729a2bb6cc941544ba2d70a9
YmendezM <ysmendezm@gmail.com>
2023-06-16 23:32:40 -0400

    Fix communication with event docker service

--- src/main/java/com/yiado/msvc/users/client/EventClientRest.java
+++ src/main/java/com/yiado/msvc/users/client/EventClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="msvc-events", url="localhost:8002")
-@FeignClient(name="msvc-events", url="host.docker-internal:8002")
+@FeignClient(name="msvc-events", url="host.docker.internal:8002")
 public interface EventClientRest {
 
     @DeleteMapping("/delete-user-event/{id}")
