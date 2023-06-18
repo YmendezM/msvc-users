e20b35e4644ab85a171d12da4be8a6ba3820e592
YmendezM <ysmendezm@gmail.com>
2023-06-18 16:16:31 -0400

    Communication between containers

--- src/main/java/com/yiado/msvc/users/client/EventClientRest.java
+++ src/main/java/com/yiado/msvc/users/client/EventClientRest.java
@@ -4,9 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+//call local
 //@FeignClient(name="msvc-events", url="localhost:8002")
-@FeignClient(name="msvc-events", url="host.docker.internal:8002")
+//call docker container -  local
+//@FeignClient(name="msvc-events", url="host.docker.internal:8002")
+//call Docker container - Docker container
+@FeignClient(name="msvc-events", url="msvc-events:8002")
 public interface EventClientRest {
 
     @DeleteMapping("/delete-user-event/{id}")
